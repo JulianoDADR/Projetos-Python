ba3ee5cce64b54dfaa94006045ad554d49dfc5fa
Juliano Dantas <102887266+JulianoDADR@users.noreply.github.com>
2024-09-09 14:14:48 -0300

    Update Banco_POO.py

--- Engenharia-de-Dados-Python na Dio/Banco resolução/Banco_POO.py
+++ Engenharia-de-Dados-Python na Dio/Banco resolução/Banco_POO.py
@@ -139,3 +139,31 @@ class Transacao(ABC):
     @abstractclassmethod
     def registrar(self, conta):
         pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    @property
+    def valor(self):
+        return self.valor
+    
+    def registrar(self, conta):    
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    @property
+    def valor(self):
+        return self.valor
+    
+    def registrar(self, conta):    
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
